03635cba355c0a766d65fb4a8359d18c9733b075
Daniele Roccaforte <64607921+YonderUrik@users.noreply.github.com>
2021-10-16 13:12:55 +0200

    completed docente classes

--- Esercizio 5 DB/src/dao/DAO.java
+++ Esercizio 5 DB/src/dao/DAO.java
@@ -70,12 +70,12 @@ public class DAO {
         }
     }
 
-    public static void deleteTuplaCorso(int id) {
+    public static void deleteTuplaCorso(String nome) {
         Connection conn1 = null;
         try {
             conn1 = (Connection) DriverManager.getConnection(url, user, password);
             Statement st = conn1.createStatement();
-            st.executeUpdate("DELETE FROM CORSO WHERE ID = "+id+"");
+            st.executeUpdate("DELETE FROM CORSO WHERE NOME = '"+nome+"'");
             st.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -92,14 +92,70 @@ public class DAO {
     }
 
     public static ArrayList<Docente> getTupleDocente(){
-
+        Connection conn1 = null;
+        ArrayList<Docente> out = new ArrayList<>();
+        try {
+            conn1 = (Connection) DriverManager.getConnection(url, user, password);
+            Statement st = conn1.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM DOCENTE");
+            while (rs.next()) {
+                Docente p = new Docente(rs.getInt("id"),rs.getString("nome"), rs.getString("cognome"));
+                out.add(p);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (conn1 != null) {
+                try {
+                    conn1.close();
+                } catch (SQLException e2) {
+                    System.out.println(e2.getMessage());
+                }
+            }
+        }
+        return out;
     }
 
     public static void setTuplaDocente(String nome, String cognome){
-
+        Connection conn1 = null;
+        try {
+            conn1 = (Connection) DriverManager.getConnection(url, user, password);
+            Statement st = conn1.createStatement();
+            st.executeUpdate("INSERT INTO corso (id, nome, cognome) VALUES (0, '" +nome+"', '"+cognome+"' )");
+            st.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (conn1 != null) {
+                try {
+                    conn1.close();
+                } catch (SQLException e2) {
+                    System.out.println(e2.getMessage());
+                }
+            }
+        }
     }
 
-    public void deleteTuplaDocente(int id){
-
+    public void deleteTuplaDocente(String nome, String cognome){
+        Connection conn1 = null;
+        try {
+            conn1 = (Connection) DriverManager.getConnection(url, user, password);
+            Statement st = conn1.createStatement();
+            st.executeUpdate("DELETE FROM CORSO WHERE NOME = '"+nome+"' AND COGNOME = '"+cognome+"'");
+            st.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (conn1 != null) {
+                try {
+                    conn1.close();
+                } catch (SQLException e2) {
+                    System.out.println(e2.getMessage());
+                }
+            }
+        }
     }
 }
